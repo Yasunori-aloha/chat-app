197cd839fba1e05c926167b5827ad840cd7384e3
Yasunori-aloha <70802846+Yasunori-aloha@users.noreply.github.com>
2020-09-14 23:32:18 +0900

    Revert "チャットルームの削除機能の実装"
    This reverts commit 72ec4d9a113c739a28133fc49f0a280798a50a73.

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -7,7 +7,7 @@ class RoomsController < ApplicationController
   end
 
   def create
-    @room = Room.new(room_params)
+    @room = Room.create(room_params)
     if @room.save
       redirect_to root_path
     else
@@ -15,15 +15,9 @@ class RoomsController < ApplicationController
     end
   end
 
-  def destroy
-    room = Room.find(params[:id])
-    room.destroy
-    redirect_to root_path
-  end
-
   private
 
   def room_params
-    params.require(:room).permit(:name, user_ids:[])
+    params.require(:room).permit(:name, user_ids: [])
   end
 end
