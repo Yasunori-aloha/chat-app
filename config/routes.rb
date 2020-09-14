d2b230ed66908e870026445e6636e1a672c0f15f
Yasunori-aloha <70802846+Yasunori-aloha@users.noreply.github.com>
2020-09-14 17:42:19 +0900

    チャット画面の編集

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "messages#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, onli: [:new, :create]
+  resources :rooms, only: [:new, :create]
 end
